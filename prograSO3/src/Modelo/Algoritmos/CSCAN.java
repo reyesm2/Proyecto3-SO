e540bf61356513db6a7c09320f67b4c519f3d756
skylar80004 <josedavidvm04@hotmail.com>
2018-11-06 15:28:56 -0600

    Algoritmo CSCAN

--- prograSO3/src/Modelo/Algoritmos/CSCAN.java
+++ prograSO3/src/Modelo/Algoritmos/CSCAN.java
@@ -5,6 +5,10 @@
  */
 package Modelo.Algoritmos;
 
+import Modelo.Requisiciones;
+import Modelo.Solicitud;
+import java.util.ArrayList;
+
 /**
  *
  * @author Rigo-PC
@@ -13,6 +17,56 @@ public class CSCAN extends SCAN{
 
     public CSCAN(String nombre) {
         super(nombre);
+    }
+    
+    
+    
+    @Override
+    public void ejecutar(Requisiciones requesicion,int inicio,int total,int direccion,int cantidad){
+        
+        
+        int cabezaDisco = inicio; 
+        
+        ArrayList<Solicitud> listaSolicitudes = requesicion.getListaSolicitudes();
+        Solicitud solicitudTemporal;
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        if(direccion == 1 ){
+            
+            while(cabezaDisco != total-1){
+                solicitudTemporal = listaSolicitudes.get(cabezaDisco);
+                result.add(solicitudTemporal.getPista());
+                cabezaDisco++;
+            }
+            
+            cabezaDisco = 0 ;
+            
+            while(cabezaDisco != total-1){
+                solicitudTemporal = listaSolicitudes.get(cabezaDisco);
+                result.add(solicitudTemporal.getPista());
+                cabezaDisco++;
+            }
+            
+        }
+        else{
+            
+            while(cabezaDisco != 0){
+                solicitudTemporal = listaSolicitudes.get(cabezaDisco);
+                result.add(solicitudTemporal.getPista());
+                cabezaDisco--;
+            }
+            
+            cabezaDisco = total-1;
+            
+            while(cabezaDisco != 0){
+                solicitudTemporal = listaSolicitudes.get(cabezaDisco);
+                result.add(solicitudTemporal.getPista());
+                cabezaDisco--;
+            }
+            
+        }
+        
+        super.agregarResultado(result);
     }
     
 }
