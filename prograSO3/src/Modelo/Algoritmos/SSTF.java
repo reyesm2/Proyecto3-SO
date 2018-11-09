63c4e3c17a04f47c3de6e50d7b6e55a76b05f40a
reyesm2 <reyes.marlon57@gmail.com>
2018-11-09 16:18:30 -0600

    SSTF Listo

--- prograSO3/src/Modelo/Algoritmos/SSTF.java
+++ prograSO3/src/Modelo/Algoritmos/SSTF.java
@@ -7,6 +7,7 @@ package Modelo.Algoritmos;
 
 import Modelo.Algoritmo;
 import Modelo.Requisiciones;
+import Modelo.Solicitud;
 import java.util.ArrayList;
 
 /**
@@ -20,8 +21,63 @@ public class SSTF extends Algoritmo{
     }
 
     @Override
-    public void ejecutar(Requisiciones requesicion,int inicio,int total,int direccion,int cantidad){
+    public void ejecutar(Requisiciones requisicion,int inicio,int total,int direccion,int cantidad){
+
+        ArrayList<Integer> result = new ArrayList<>();
+
         
+        ArrayList<Solicitud> listaSolicitud = (ArrayList<Solicitud>) requisicion.getListaSolicitudes().clone();
+        
+        for(int i=0;i<requisicion.getListaSolicitudes().size();i++){
+            
+            ArrayList<Integer> diferencias = calcularDiferencias(listaSolicitud, inicio);
+            
+            System.out.println("Inicio: "+inicio);
+            System.out.println("Diferencias: "+diferencias.toString());
+            System.out.println(listaSolicitud.toString());
+            
+            int pos = posNumeroMenor(diferencias);
+            
+            Solicitud solicitud = listaSolicitud.remove(pos);
+            
+            result.add(solicitud.getPista());
+            inicio = solicitud.getPista();
+
+        }
+        super.agregarResultado(result);
     }
     
+    public int valorAbsoluto (int numero) {
+      return numero > 0 ? numero : -numero;
+    }
+    
+    public ArrayList<Integer> calcularDiferencias(ArrayList<Solicitud> listaSolicitud,int inicial){
+        ArrayList<Integer> result = new ArrayList<>();
+        for(Solicitud solicitud : listaSolicitud){
+            int valor = valorAbsoluto(solicitud.getPista()-inicial);
+            result.add(valor);
+        }
+        return result;
+    }
+    
+    /**
+     * Retorna la posicion del mayor
+     * @param lista
+     * @return 
+     */
+    public int posNumeroMenor(ArrayList<Integer> lista){
+        int menor=lista.get(0);
+        int i =0;
+        int result=0;
+        for(i=0;i<lista.size();i++){
+            
+            if(menor>=lista.get(i)){
+                menor=lista.get(i);
+                result=i;
+
+            }
+ 
+        }
+        return result;
+    }
 }
